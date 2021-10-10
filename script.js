2767a348d48ebb131c903b16bb120d5fd08223aa
camiloc8460 <camiloc8460@hstat.org>
2021-10-10 15:18:01 -0400

    homescreendone

--- script.js
+++ script.js
@@ -1,26 +1,5 @@
-/*let url = "https://api.nasa.gov/planetary/apod?api_key=ToCpyJhAnXU1y9pI4phfWI8S27Lkn9qnZGqu8LdB";
-
-fetch(url).then(
-    function(response){
-        console.log(response);
-        return response.json();
-    }
-).then(
-    function(json){
-        
-        let explain = document.createElement("h1");
-        
-        explain.innerHTML = json.explanation;
-        
-        explain.style["font-family"] = "Garamond";
-        explain.style["color"] = "black";
-       
-        document.body.appendChild(explain);
-        
-        console.log(json);
-    }
-) */
-
 
     
-     
+
+
+
