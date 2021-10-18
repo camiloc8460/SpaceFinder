95e89bc7a0c92918b906f51fea813644d960feb9
camiloc8460 <camiloc8460@hstat.org>
2021-10-18 18:36:12 -0400

    sortofworks

--- script2.js
+++ script2.js
@@ -44,7 +44,13 @@ function useApiData (data) {
     document.querySelector("#content3").style.textAlign = "center";
     document.querySelector("#content3").style.color = "white";
     
-    document.querySelector("#content4").style.position = "absolute";
-    document.querySelector("#content4").style.marginRight = "auto";
     
 }
+
+const menuToggle = document.querySelector('.toggle')
+          const showcase = document.querySelector('.showcase')
+
+            menuToggle.addEventListener('click', () => {
+                menuToggle.classList.toggle('active')
+                showcase.classList.toggle('active')
+        })
